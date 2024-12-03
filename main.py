db9aa281b62f219ff79594d0307df70ca7314e26
Gleb <glebpetrof2007@gmail.com>
2024-12-03 12:30:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-unique_words=set()
-with open('text.txt','r') as infile:
+unique_words = set() #создание пустого множества уникальных слов
+with open('text.txt','r') as infile: #чтенике из файла и проверка на уникальность
     for line in infile:
         words=line.split()
         for word in words:
             unique_words.add(word)
-with open('output.txt','w') as outfile:
+with open('output.txt','w') as outfile: #запись в файл
     for word in unique_words:
         outfile.write(word+' ')
